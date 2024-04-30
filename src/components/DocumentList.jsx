dabe2d3e3aeccffd8eab419225476abc03741c2e
ahmed-zetabox <ahmed.abdelmoula@zeta-box.com>
2024-04-30 13:06:39 +0100

    testing commit

--- src/components/DocumentList.jsx
+++ src/components/DocumentList.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import { getDocuments } from "../js/actions/documentActions";
-import { useDispatch, useSelector } from "react-redux";
-import { serverURL } from "../utils/endpoints";
+import { getUser } from "../js/actions/userActions";
 
 const DocumentList = () => {
   const params = useParams();
@@ -19,12 +18,16 @@ const DocumentList = () => {
       )
       .flatMap((el) => el)
   );
-  console.log(localStorage.getItem("userId"));
-  console.log("connected user department", authUser.department);
+
+  useEffect(() => {
+    dispatch(getUser(localStorage.getItem("userId")));
+    dispatch(getDocuments(params.department));
+  }, [])
+  
 
   useEffect(() => {
     console.log("params changed", documents);
-
+    console.log("docs",documents?.filter(el=>permittedUsers.includes(authUser.username)))
     setPermittedUsers(
       documents
         .map((el) =>
@@ -57,21 +60,26 @@ const DocumentList = () => {
   );
 
   return (
-    <div>
-      <ul>
-        {authUser?.documents?.map((document) => (
-          <li key={document.id}>
-            <Link
-              className="text-blue-800 underline underline-offset-1 font-semibold"
-              to={`/document/${document.id}`}
-            >
-              {document?.Title}
-              {/* {`${serverURL}${document?.attributes?.file?.attributes?.url}`} */}
-              {/* See pdf file */}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="flex-col" >
+      <div className="text-3xl items-start font-bold">
+          <h1 className="felx-start left-0" > {params.department} Documents  </h1>
+      </div>
+      <div>
+        <ul>
+          {documents?.filter(el=>permittedUsers.includes(authUser.username)).map((document) => (
+            <li key={document.id}>
+              <Link
+                className="text-blue-800 underline underline-offset-1 font-semibold"
+                to={`/document/${document.id}`}
+              >
+                {document?.attributes?.Title}
+                {/* {`${serverURL}${document?.attributes?.file?.attributes?.url}`} */}
+                {/* See pdf file */}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
